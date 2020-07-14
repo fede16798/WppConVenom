4695a763a57e0507d21189add08d3a11e63ee05e
Fede Amico <fechee67@gmail.com>
2020-07-14 11:23:36 -0300

    Capturando el nombre de contacto para personalizar los mensjaes

--- moduleVenom.js
+++ moduleVenom.js
@@ -1,18 +1,28 @@
 // Supports ES6
 //import { create, Whatsapp } from 'venom-bot';
 const venom = require("venom-bot");
-//numeroAEnviar debe ser un string con el siguiente formato 'numero@c.us'
-let numeroAEnviar = "null";
+//numeroAEnviar debe ser un string con el siguiente formato 'cod area + numero@c.us'
+let numeroAEnviar = "5491112345678@c.us";
+//mencion es un strin y solo tiene el cod area + numero de la persona a la que se quiere mencionar luego
+let mencion = '5491112345678'
 
 async function creador() {
   //Creo la sesión en WhatsApp Web
     const cliente = await venom.create();
-    /* Apenas creada la sesión, se envía un mensaje al numeroAEnviar que es el ID de la persona. */
+    /*Apenas creada la sesión, se envía un mensaje al numeroAEnviar que es el ID de la persona. */
     const enviarMensaje = await cliente.sendText(numeroAEnviar, primerMensaje);
+    /*Envio un mensaje mencionando al reminente. En este caso es necesario agregar el @ para poder capturar el nombre*/
+    const enviarMencion = await cliente.sendMentioned(numeroAEnviar, `Hola @${mencion} te hablo desde un bot que hice`, [
+      mencion
+    ]);
 
     console.log(enviarMensaje.toString())
     /* Si recibo un mensaje lo comparo y respondo. message.from Es el ID del chat al cual le debo responder. */
     cliente.onMessage((message) => {
+      console.log(message)
+      //Capturo el nombre de la persona cuando recibo un mensaje
+      let nombre = message.sender.pushname;
+      cliente.sendText(message.from, `Hola ${nombre}`);
       let mensajeRecibido = message.body.toLowerCase();
       if (mensajeRecibido === "info" || mensajeRecibido === "información") {
         cliente.sendText(message.from, respuestaAutomatica);
--- test.js
+++ test.js
@@ -1,6 +1,6 @@
-//const venom = require('./moduleVenom')
-const venom = require('./prueba')
+const venom = require('./moduleVenom')
+//const venom = require('./prueba')
+
 
 venom.creador();
-//venom.enviarMensaje();
 
